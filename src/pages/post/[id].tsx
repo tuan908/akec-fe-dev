9e9be7d23872f8b71698e703e4cc590cd3597d1d
Tuan Nguyen Anh <tuanna@aureole-it.vn>
2023-08-09 11:20:00 +0700

    add post detail demo page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,7 +1,7 @@
 export default function PostDetailSample() {
   return (
     <div className='grid grid-cols-2'>
-      <div>Image</div>
+      <div>Image Test</div>
       <div>Post content</div>
     </div>
   )
